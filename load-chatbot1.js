302bb8dd1d6679071ae440bcf13d8cfa3793f354
Shreya-7558 <kurodeshreya123@gmail.com>
2025-04-30 10:52:16 +0530

    changes done

--- load-chatbot1.js
+++ load-chatbot1.js
@@ -34,8 +34,8 @@ if (chatbotContainer) {
   iframe.style.position = "fixed";
   iframe.style.bottom = "20px";
   iframe.style.right = "20px";
-  iframe.style.width = "400px";
-  iframe.style.height = "600px";
+  iframe.style.width = "130px";
+  iframe.style.height = "130px";
   iframe.style.border = "none";
   iframe.style.borderRadius = "12px";
   iframe.style.boxShadow = "0 0 10px rgba(0, 0, 0, 0)";
